2c425aaea19e631f5252e26496941ee7c649e712
devMasrafi <masrafisw@gmail.com>
2023-12-16 22:38:50 +0600

    Inquire form added

--- src/pages/property_details/PropertyDetails.jsx
+++ src/pages/property_details/PropertyDetails.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PropertyBanner from "../../sections/property_details/PropertyBanner";
 import PropertyDisc from "../../sections/property_details/PropertyDisc";
+import Inquire from "../../sections/property_details/Inquire";
 
 const Propertydetails = () => {
   return(
     <>
       <PropertyBanner />
       <PropertyDisc/>
+      <Inquire/>
     </>
   );
 };
