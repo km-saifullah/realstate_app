8f960dca740f5589f9ff418cb3370ded0b129b6c
mizanurrrahaman <rodking1414@gmail.com>
2023-12-16 05:36:13 -0800

    achievements section done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./about.css";
 import Journey from "../../sections/journey/Journey";
 import Values from "../../sections/values/Values";
-
+import Achievements from "../../sections/achievements/Achievements"
 const About = () => {
   return(
       <>
       <Journey/>
       <Values/>
+      <Achievements/>
       </>
   ) 
 };
