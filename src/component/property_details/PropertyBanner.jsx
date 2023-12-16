0dcad1af020e1396f6df52edc9575c8f236ba919
devMasrafi <masrafisw@gmail.com>
2023-12-16 14:46:28 +0600

    image files added and css OTW

---
+++ src/component/property_details/PropertyBanner.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './propertyBanner.css'
+import '../../../src/index.css'
+import { FaLocationDot } from "react-icons/fa6";
+
+const PropertyBanner = () => {
+  return (
+    <section className="property_banner">
+        <div className="container">
+            <div className="seaside_villa">
+                <div className="header">
+                    <div className="header_main">
+                        <h2>Seaside Serenity Villa</h2>
+                        <div className="locaton">
+                            <FaLocationDot />
+                            <p>Malibu, California</p>
+                        </div>
+                    </div>
+                    <div className="header_price">
+                        <p>price</p>
+                        <h2>$1,200,000</h2>
+                    </div>
+                </div>
+                <div className="img_section">
+                    <div className="img_selection">
+
+                    </div>
+                    <div className="main_img">      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default PropertyBanner
--- src/pages/property_details/PropertyDetails.jsx
+++ src/pages/property_details/PropertyDetails.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PropertyBanner from '../../component/property_details/PropertyBanner'
 
 const propertydetails = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <PropertyBanner/>
+    </>
   )
 }
 
