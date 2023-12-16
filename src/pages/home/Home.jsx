6e15663bb38c1d73e56880e710a04de35c8da060
km-saifullah <kmsaifullah16@gmail.com>
2023-12-16 20:21:06 +0600

    banner section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./home.css";
+import Banner from "../../sections/banner/Banner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Banner />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/sections/banner/Banner.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "../../components/img/Image";
+import bannerImg from "/banner_img.png";
+import "./banner.css";
+import CommonBtn from "../../components/commonBtn/CommonBtn";
+
+const Banner = () => {
+  return (
+    <section className="banner">
+      <div className="container">
+        <div className="banner_wrapper">
+          <div className="banner_info">
+            <div className="banner_head-para">
+              <h1 className="banner_heading">
+                Discover Your Dream Property with Estatein
+              </h1>
+              <p className="banner_text">
+                Your journey to finding the perfect property begins here.
+                Explore our listings to find the home that matches your dreams.
+              </p>
+            </div>
+            <div className="banner_links">
+              <CommonBtn className="banner_btn" text="Learn More" />
+              <CommonBtn className="banner_btn" text="Browser Property" />
+            </div>
+            <div className="banner_card">
+              <div className="banner_item">
+                <h3>200+</h3>
+                <p>Happy Customers</p>
+              </div>
+              <div className="banner_item">
+                <h3>10k+</h3>
+                <p>Properties For Clients</p>
+              </div>
+              <div className="banner_item">
+                <h3>16+</h3>
+                <p>Years of Experience</p>
+              </div>
+            </div>
+          </div>
+          <div className="banner_img">
+            <Image
+              className="banner_thumbnail"
+              imgSrc={bannerImg}
+              altText="Banner Image"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
