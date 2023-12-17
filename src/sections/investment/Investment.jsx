7952942eaf2d316141f4cccd4d133bfd95620f72
mizanurrrahaman <rodking1414@gmail.com>
2023-12-16 23:01:15 -0800

    investment section done

---
+++ src/sections/investment/Investment.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./investment.css"
+
+
+const Investment = () => {
+  return (
+    <section className="investment">
+       <div className="container">
+           <div className="investment-wrapper">
+             <div className="investment-item">
+               <h2>Start Your Real Estate Journey Today</h2>
+               <p>Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is here to assist you every step of the way. Take the first step towards your real estate goals and explore our available properties or get in touch with our team for personalized assistance.</p>
+             </div>
+              <div className="investment-btn">
+              <button className='from-btn'>Explore Properties</button>
+              </div>
+           </div>
+       </div>
+    </section>
+  )
+}
+
+export default Investment
