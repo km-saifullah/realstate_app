0d9b33a30b1c75d10e28901ccac9c2cb2e0589c1
km-saifullah <kmsaifullah16@gmail.com>
2023-12-17 20:50:41 +0600

    featured properties section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import "./home.css";
 import Banner from "../../sections/banner/Banner";
 import Service from "../../sections/service/Service";
 import RealState from "../../sections/realstate/RealState";
+import FeaturedProperties from "../../sections/featuresProperties/clientsSaying/FeaturedProperties";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Service />
+      <FeaturedProperties />
       <RealState />
     </>
   );
