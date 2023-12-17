d44599b1300bdea72636a42943c56d75c0ba46c8
devMasrafi <masrafisw@gmail.com>
2023-12-17 17:29:41 +0600

    added pricing header

---
+++ src/sections/property_details/PricingDetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './pricingDetails.css'
+
+const PricingDetails = () => {
+  return (
+    <section className='pricingDetails'>
+        <div className="container">
+            <div className="pricingDetials_headder">
+                <h2>Comprehensive Pricing Details</h2>
+                <p>At Estatein, transparency is key. We want you to have a clear understanding of all costs associated with your property investment. Below, we break down the pricing for Seaside Serenity Villa to help you make an informed decision</p>
+            </div>
+            <div className="pricingDetails_note">
+                <h2>Note :</h2>
+                <p>The figures provided above are estimates and may vary depending on the property, location, and individual circumstances.</p>
+            </div>
+            <div className="pricingDetails_wrapper">
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default PricingDetails
