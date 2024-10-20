8a46cdfb607b55eadb1fa8d4361a5aa53601aed6
dohabristy <146329970+dohabristy@users.noreply.github.com>
2024-10-20 15:57:26 -0400

    form part done

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import "./contact.css"
 const Contact = () => {
   return (
@@ -9,8 +9,9 @@ const Contact = () => {
               <h3>
                   Get in Touch with Estatein
               </h3>
-              <p>Welcome to Estatein's Contact Us page. We're here to assist you with any inquiries, requests, or
-              feedback you may have. Whether you're looking to buy or sell a property, explore investment opportunities, or simply want to connect, we're just a message away. Reach out to us, and let's start a conversation.</p>
+              <p>Welcome to Estateins Contact Us page. Were here to assist you with any inquiries, requests, or
+              feedback you may have. Whether youre looking to buy or sell a property, explore investment opportunities,
+               or simply want to connect, were just a message away. Reach out to us, and lets start a conversation.</p>
           </div>
         </section>
         <section id='contact_part_two'>
@@ -38,11 +39,68 @@ const Contact = () => {
 
         <section id='contact_part_3'>
           <div className='three-wrap container'>
-            <h3>Let's Connect</h3>
-            <p>We're excited to connect with you and learn more about your 
-              real estate goals. Use the form below to get in touch with Estatein. Whether you're a prospective client, partner, or simply curious about our services, we're here to answer your questions and provide the assistance you need.</p>
+            <img src='/star.png'/>
+            <h3>Lets Connect</h3>
+            <p> Were excited to connect with you and learn more about your 
+              real estate goals. Use the form below to get in touch with Estatein. Whether youre a
+               prospective client, partner, or simply curious about our services, were here to answer your questions and provide
+               the assistance you need.</p>
           </div>
-        </section>
+
+          <div className="form_box container">
+            <div className="form-wrap ">
+              <div className="form_wrapper">
+                <div className="form">
+                  <label>First Name</label>
+                  <input type="text"  name="firstname" placeholder="Your name.."/>
+                </div>
+                <div className="form">
+                  <label>First Name</label>
+                  <input type="text"  name="firstname" placeholder="Your name.."/>
+                </div>
+
+      
+              
+              </div>
+
+              <div className="form_wrapper">
+                <div className="form">
+                  <label>First Name</label>
+                  <input type="text"  name="firstname" placeholder="Your name.."/>
+                </div>
+                <div className="form">
+                  <label>First Name</label>
+                  <input type="text"  name="firstname" placeholder="Your name.."/>
+                </div>
+
+      
+              
+              </div>
+
+              <div className="form_wrapper">
+                <div className="form">
+                  <label>First Name</label>
+                  <input type="text"  name="firstname" placeholder="Your name.."/>
+                </div>
+                <div className="form">
+                  <label>First Name</label>
+                  <input type="text"  name="firstname" placeholder="Your name.."/>
+                </div>
+
+      
+              
+              </div>
+
+            </div>
+              <div className="form massagebox">
+                <label>First Name</label>
+                <input type="text"  name="firstname" placeholder="Your name.."/>
+              </div>
+          </div>
+          
+
+          
+        </section> 
 
 
       </div>
