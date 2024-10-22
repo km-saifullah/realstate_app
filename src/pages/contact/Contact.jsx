4f0c243f39c7588eb9b948fb1df4227238609142
dohabristy <146329970+dohabristy@users.noreply.github.com>
2024-10-22 05:01:26 -0400

    sec4 done

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -97,10 +97,57 @@ const Contact = () => {
                 <input type="text"  name="firstname" placeholder="Your name.."/>
               </div>
           </div>
-          
-
-          
         </section> 
+
+        <section id="conatct_part_4">
+          <div className='three-wrap container'>
+            
+              <img src='/star.png'/>
+              <h3>Discover Our Office Locations</h3>
+              <p>Estatein is here to serve you across multiple locations. Whether youre looking to meet our team, discuss 
+                real estate opportunities, or simply drop by for a chat, we have offices conveniently located to serve your needs.
+                 Explore the categories below to find the Estatein office nearest to you.</p>
+          </div>
+          <div className="container contact_btn_wrap">
+            <button className="contact_btn"> All</button>
+            <button className="contact_btn"> All</button>
+            <button className="contact_btn"> All</button>
+          </div>
+
+          <div className="location_box_wrap container">
+            <div className="location_box">
+             <p className="main">Main Headquarters</p>
+              <h3>123 Estatein Plaza, City Center, Metropolis</h3>
+              <p className="our">Our main headquarters serve as the heart of Estatein. Located in the bustling city center, this is where our 
+                core team of experts operates, driving the excellence and innovation that define us.</p>
+
+                <div>
+                  <div className="location_btn">
+                    <button>info@estatein.com</button>
+                    <button>+1 (123) 456-7890</button>
+                    <button>Metropolis</button>
+                  </div>
+                </div>
+                <button className="get_btn">Get Direction</button>
+            </div>
+            
+            <div className="location_box">
+             <p className="main">Regional Offices</p>
+              <h3>456 Urban Avenue, Downtown District, Metropolis</h3>
+              <p className="our">Estatein's presence extends to multiple regions, each with its own dynamic real 
+                estate landscape. Discover our regional offices, staffed by local experts who understand the nuances of their respective markets.</p>
+
+                <div>
+                  <div className="location_btn">
+                    <button>info@estatein.com</button>
+                    <button>+1 (123) 456-7890</button>
+                    <button>Metropolis</button>
+                  </div>
+                </div>
+                <button className="get_btn">Get Direction</button>
+            </div>
+          </div>
+        </section>
 
 
       </div>
